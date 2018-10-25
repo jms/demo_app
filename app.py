8907f755518c89d0fcb69a23f78e5d9082e729be
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2018-10-25 08:08:11 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
